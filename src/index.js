06084545c2f3c4333340299bf676f5d3168e56fd
Valentin Duffet <vavadu07@gmail.com>
2022-01-11 16:05:19 +0100

    add react query

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import Header from './Components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.render(
   <BrowserRouter>
+
     <Header/>
-    <App />
+    
+    <QueryClientProvider client={queryClient}>
+	    <App />
+    </QueryClientProvider>
+
   </BrowserRouter>,
   document.getElementById('root')
 );
